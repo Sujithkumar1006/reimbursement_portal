43872fdceee070a1a7a18f1ed6a209186e4d7a67
Sujith Kumar <sujithkumar.t10@gmail.com>
2026-03-13 12:41:04 -0400

    fix: bills model validation added

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,4 +2,6 @@ class Bill < ApplicationRecord
   belongs_to :user
   enum :bill_type, { food: 0, travel: 1, others: 2 }
   enum :status, { approved: 0, pending: 1, rejected: 2 }
+  validates :bill_type, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
