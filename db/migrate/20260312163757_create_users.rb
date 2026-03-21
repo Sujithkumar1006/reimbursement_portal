fc6322c605f6faa52ad139c4207a2ef8f44df57b
Sujith Kumar <72454536+Sujithkumar1006@users.noreply.github.com>
2026-03-21 11:07:13 -0400

    Update 20260312163757_create_users.rb

--- db/migrate/20260312163757_create_users.rb
+++ db/migrate/20260312163757_create_users.rb
@@ -4,7 +4,8 @@ class CreateUsers < ActiveRecord::Migration[8.1]
       t.string :name
       t.string :email
       t.string :password_digest
-      t.integer :role, default: 0
+      
+      t.integer :role, default: 1
       t.timestamps
     end
     add_index :users, :email, unique: true
